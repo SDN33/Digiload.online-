d53dc39ce186270a2bd5616d1330791e05ae92ae
SDN33 <s.deinegri2@gmail.com>
2024-09-11 16:49:56 +0200

    canvapro

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.tsx
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-purple-400 text-white p-4 text-center">
+    <footer className="bg-purple-700 text-white p-4 text-center">
       <p>&copy; {new Date().getFullYear()} Still-inov Agency. Tout droits réservés.</p>
     </footer>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image';
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-purple-400 to-blue-500 text-white">
+    <div className="flex flex-col min-h-screen bg-gradient-to-r from-purple-700 to-blue-500 text-white">
       <Header />
       <main className="flex flex-1 flex-col md:flex-row">
         {/* Section gauche avec le bouton */}
         <div className="flex-1 flex items-center justify-center p-8">
           <div className="text-center space-y-4 md:space-y-8">
-            <h1 className="text-4xl font-bold mb-8">Decouvrez Canva Pro Gratuitement</h1>
+            <h1 className="text-4xl font-bold mb-8">Decouvrez<br />Canva Pro<br />Gratuitement</h1>
             <Link href="https://www.canva.com/pro">
               <a
                 className="bg-white text-blue-700 font-bold py-3 px-6 rounded-lg shadow-lg hover:bg-gray-100"
