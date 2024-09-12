4e12c1b2b6a0e30a2dd108970ec0c4376612c0d7
SDN33 <s.deinegri2@gmail.com>
2024-09-12 16:58:31 +0200

    canvapro

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const Home: React.FC = () => {
                 height={100}
                 objectFit="contain"
               />
-              <h1 className="text-xl md:text-2xl font-bold leading-tight -mt-12">
+              <h1 className="text-xl md:text-2xl font-bold leading-tight -mt-16">
                 La communauté des utilisateurs de Canva
               </h1>
             </div>
