128450bbacdbb283bf0ca69a516c43314c60fab6
SDN33 <s.deinegri2@gmail.com>
2024-09-12 16:30:16 +0200

    canvapro

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
             </h1>
             {/* Adding the Canva logo with white background */}
             <div className="flex justify-center">
-              <div className="bg-white rounded-full p-4">
+              <div className="bg-white rounded-full">
                 <Image
                   src="/images/logocanva.png"
                   alt="Canva Logo"
