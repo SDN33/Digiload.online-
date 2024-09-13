640fc24d5c37bb51d26611cb44a0431d7c950140
SDN33 <s.deinegri2@gmail.com>
2024-09-13 13:30:07 +0200

    canvapro

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useState, useEffect, useCallback } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import Header from './components/Header';
 import Footer from './components/Footer';
