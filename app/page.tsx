51ad875497d4a8408f7280e65de77f07fff950f6
SDN33 <s.deinegri2@gmail.com>
2024-09-19 16:48:26 +0200

    digiload

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ const Home: React.FC = () => {
               className="btn bg-white text-blue-700 font-bold py-3 px-6 rounded-lg shadow-lg hover:bg-gray-400 transition duration-300 ease-in-out"
               onClick={() => handleStepClick(1)}
             >
-              Canva Pro Gratuit
+              Cliquez ici pour la pub
             </button>
             {adClickCount >= 3 && (
               <button
