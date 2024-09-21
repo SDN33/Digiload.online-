b834258ed816187806f2b7f65634edb176630106
SDN33 <s.deinegri2@gmail.com>
2024-09-21 16:45:51 +0200

    dl

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,7 +14,6 @@ const Footer: React.FC = () => {
   return (
     <>
       <footer className="bg-white text-black p-4 text-center relative">
-      <p className="text-sm mt-2">Notre site est 100% sécurisé et ne nécessite aucune information personnelle</p>
 
       <button
           onClick={handleLegalClick}
