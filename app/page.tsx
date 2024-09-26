f431678dbc4272f3063966c7ed095dae98f86343
SDN33 <s.deinegri2@gmail.com>
2024-09-26 14:17:30 +0200

    dl

--- app/page.tsx
+++ app/page.tsx
@@ -119,7 +119,7 @@ const Home: React.FC = () => {
 
       <br />
 
-      <div className="border-t border-gray-200 py-4 text-center"></div>
+      <div className="absolute top-0 left-0 w-full h-[3px] bg-gradient-to-r from-[#7d2ae7] via-[#6420ff] to-[#00c4cc]"></div>
 
       <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-700 to-blue-500 bg-cover bg-center bg-no-repeat text-white overflow-x-hidden animate-gradient-x">
         <motion.div
